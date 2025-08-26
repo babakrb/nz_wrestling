3a7f69d70f0f98fa4df6bfc43030a87a7d2c9151
babakrb <babak.rb@gmail.com>
2025-08-26 12:10:11 +1200

    Update Dashboard.jsx

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
   const goToEditCompetition = () => {
     navigate('/EditCompetition');
   };
-console.log(`Front=${process.env.REACT_APP_FRONTEND_URL}`);
+//console.log(`Front=${process.env.REACT_APP_FRONTEND_URL}`);
 
   return (
     <div className="container my-5">
